abb7ea85638dfa2ab3dda398f1da1e74d3a1ed75
RadekKisiela <radek1997_97@o2.pl>
2023-12-01 21:30:55 +0100

    code was refactored into hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,71 +2,61 @@ import Statistics from 'components/statistics/Statistics';
 import Section from './section/Section';
 import Notification from './notification/Notification';
 import FeedbackOptions from './feedbackoptions/FeedbackOptions';
-import React from 'react';
+import React, { useState } from 'react';
 
-export class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      good: 0,
-      neutral: 0,
-      bad: 0,
-    };
-  }
+export const App = () => {
+  const [state, setState] = useState({ good: 0, neutral: 0, bad: 0 });
 
-  countTotalFeedback = () => {
-    const { good, neutral, bad } = this.state;
+  const countTotalFeedback = () => {
+    const { good, neutral, bad } = state;
     return good + neutral + bad;
   };
 
-  countPositiveFeedbackPercentage = () => {
-    const { good } = this.state;
-    const total = this.countTotalFeedback();
+  const countPositiveFeedbackPercentage = () => {
+    const { good } = state;
+    const total = countTotalFeedback();
     return total > 0 ? Math.round((good / total) * 100) : 0;
   };
 
-  handleFeedback = option => {
-    this.setState(prevState => ({
-      ...prevState,
-      [option]: prevState[option] + 1,
-    }));
+  const handleFeedback = option => {
+    setState(prevState => ({ ...prevState, [option]: prevState[option] + 1 }));
   };
-  render() {
-    const { good, neutral, bad } = this.state;
-    const totalFeedback = this.countTotalFeedback();
-    const positiveFeedbackPercentage = this.countPositiveFeedbackPercentage();
-    return (
-      <div
-        style={{
-          height: '100vh',
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-          fontSize: 40,
-          color: '#010101',
-        }}
-      >
-        <Section title="Please Leave Feedback">
-          <FeedbackOptions
-            options={['good', 'neutral', 'bad']}
-            onLeaveFeedback={this.handleFeedback}
+
+  const { good, neutral, bad } = state;
+  const totalFeedback = countTotalFeedback();
+  const positiveFeedbackPercentage = countPositiveFeedbackPercentage();
+
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101',
+      }}
+    >
+      <Section title="Please Leave Feedback">
+        <FeedbackOptions
+          options={['good', 'neutral', 'bad']}
+          onLeaveFeedback={handleFeedback}
+        />
+      </Section>
+      <Section title="Statistics">
+        {totalFeedback > 0 ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={totalFeedback}
+            positivePercentage={positiveFeedbackPercentage}
           />
-        </Section>
-        <Section title="Statistics">
-          {totalFeedback > 0 ? (
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={totalFeedback}
-              positivePercentage={positiveFeedbackPercentage}
-            />
-          ) : (
-            <Notification message="No feedback given" />
-          )}
-        </Section>
-      </div>
-    );
-  }
-}
+        ) : (
+          <Notification message="No feedback given" />
+        )}
+      </Section>
+    </div>
+  );
+};
--- src/components/statistics/Statistics.jsx
+++ src/components/statistics/Statistics.jsx
@@ -2,22 +2,17 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import css from './Statistics.module.css';
 
-class Statistics extends React.Component {
-  render() {
-    const { good, neutral, bad, total, positivePercentage } = this.props;
-    return (
-      <div className={css.statsWrapper}>
-        <p className={css.statsData}>Good: {good}</p>
-        <p className={css.statsData}>Neutral: {neutral}</p>
-        <p className={css.statsData}>Bad: {bad}</p>
-        <p className={css.statsData}>Total: {total}</p>
-        <p className={css.statsData}>
-          Positive feedback: {positivePercentage}%
-        </p>
-      </div>
-    );
-  }
-}
+const Statistics = ({ good, neutral, bad, total, positivePercentage }) => {
+  return (
+    <div className={css.statsWrapper}>
+      <p className={css.statsData}>Good: {good}</p>
+      <p className={css.statsData}>Neutral: {neutral}</p>
+      <p className={css.statsData}>Bad: {bad}</p>
+      <p className={css.statsData}>Total: {total}</p>
+      <p className={css.statsData}>Positive feedback: {positivePercentage}%</p>
+    </div>
+  );
+};
 
 Statistics.propTypes = {
   good: PropTypes.number.isRequired,
